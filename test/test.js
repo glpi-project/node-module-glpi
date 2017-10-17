eac65b00627989bdacecdca6c50e2f6295a53bb9
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-17 19:14:12 -0400

    ci(circle): run test with npm

--- test/test.js
+++ test/test.js
@@ -1,9 +1,5 @@
-const chaiAsPromised = require("chai-as-promised")
-const chai = require("chai")
- 
-chai.use(chaiAsPromised)
-const assert = chai.assert
-const expect  = chai.expect
+const assert = require("chai").assert
+const expect  = require("chai").expect
 
 const GlpiRestClient = require('../lib/restclient')
 const config = require('../config.json')
