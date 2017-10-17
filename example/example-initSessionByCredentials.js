21f3c7efb754e2cdf7dd878e82e3b0270dffbce3
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-17 15:02:07 -0400

    refactor(initSessionByCredentials): add App-Token as a functional parameter

--- example/example-initSessionByCredentials.js
+++ example/example-initSessionByCredentials.js
@@ -5,19 +5,10 @@ const config = require('../config.json')
 
 const client = new GlpiRestClient(config.apirest)
 
-client.initSessionByCredentials(config.user.name, config.user.password, config.user.password)
+client.initSessionByCredentials(config.user.name, config.user.password)
 	.then((res) => {
 		console.log(res)
 	})
 	.catch((err) => {
 		console.log(err)
 	})
-
-/*
-var computer = client.getAnItem('computer', 1)
-var computers = client.getMultipleItems([
-	{"itemtype": "computer", "id": 1},
-	{"itemtype": "computer", "id": 3},
-	{"itemtype": "computer", "id": 4}
-])
-*/
