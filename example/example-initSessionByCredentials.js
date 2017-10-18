4c1ef1eb43b7f5c8701cfc6e9e21a6a07f01cc8d
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-18 16:58:33 -0400

    refactor(restclient): improve the request data of the class

--- example/example-initSessionByCredentials.js
+++ example/example-initSessionByCredentials.js
@@ -6,7 +6,6 @@ const client = new GlpiRestClient(config.apirest)
 client.initSessionByCredentials(config.user.name, config.user.password, config.appToken)
 	.then((res) => {
 		console.log(res)
-		console.log(client)
 	})
 	.catch((err) => {
 		console.log(err)
