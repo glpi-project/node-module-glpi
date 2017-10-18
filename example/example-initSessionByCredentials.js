9718398a663418a1736113db35a5e3101001a278
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-18 14:05:40 -0400

    feat(sessionToken): add the session Token when login

--- example/example-initSessionByCredentials.js
+++ example/example-initSessionByCredentials.js
@@ -6,6 +6,7 @@ const client = new GlpiRestClient(config.apirest)
 client.initSessionByCredentials(config.user.name, config.user.password, config.appToken)
 	.then((res) => {
 		console.log(res)
+		console.log(client)
 	})
 	.catch((err) => {
 		console.log(err)
