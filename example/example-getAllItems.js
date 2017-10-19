126746a27426169680b007b11ee3e954c1f6f96d
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-19 18:01:25 -0400

    docs(examples): add code example of getAllItems

---
+++ example/example-getAllItems.js
@@ -0,0 +1,19 @@
+const GlpiRestClient = require('../lib/restclient')
+const config = require('../config.json')
+const itemtype = require('../lib/itemtype')
+
+const client = new GlpiRestClient(config.apirest)
+
+client.initSessionByCredentials(config.user.name, config.user.password, config.appToken)
+	.then((res) => {
+		client.getAllItems(itemtype.User)
+			.then((res2) => {
+				console.log(res2)
+			})
+			.catch((err2) => {
+				console.log(err2)
+			})
+	})
+	.catch((err) => {
+		console.log(err)
+	})
