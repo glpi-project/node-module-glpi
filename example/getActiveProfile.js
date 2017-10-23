2806a6a1616e3e878f337af9d1101466eb32b8f4
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-23 14:23:02 -0400

    docs(examples): remove the prefix 'example-' in file names

--- example/getActiveProfile.js
+++ example/getActiveProfile.js
@@ -1,14 +1,20 @@
 const GlpiRestClient = require('../lib/restclient')
 const config = require('../config.json')
-const itemtype = require('../lib/itemtype')
 
 const client = new GlpiRestClient(config.apirest)
 
 client.initSessionByCredentials(config.user.name, config.user.password, config.appToken)
 	.then((res) => {
-		client.getAnItem(itemtype.User, 2)
+		client.getActiveProfile()
 			.then((res2) => {
 				console.log(res2)
+				client.killSession()
+					.then((res3) => {
+						console.log(res3)
+					})
+					.catch((err3) => {
+						console.log(err3)
+					})
 			})
 			.catch((err2) => {
 				console.log(err2)
