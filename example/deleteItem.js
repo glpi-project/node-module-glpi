07073fdcc8d55a594eae4092490e2c584af1ce95
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-23 15:24:12 -0400

    docs(examples): add code example of deleteItem

--- example/deleteItem.js
+++ example/deleteItem.js
@@ -7,12 +7,19 @@ const client = new GlpiRestClient(config.apirest)
 client.initSessionByCredentials(config.user.name, config.user.password, config.appToken)
 	.then((res) => {
 		client.addItem(itemtype.UserEmail ,{users_id: 37, email: 'example@email.com'})
-			.then((res2) => {
-				console.log(res2)
-			})
-			.catch((err2) => {
-				console.log(err2)
-			})
+		.then((res2) => {
+			console.log(res2)
+			client.deleteItem(itemtype.UserEmail, res2.data.id, {users_id: 37, email: 'example@email.com'})
+				.then((res2) => {
+					console.log(res2)
+				})
+				.catch((err2) => {
+					console.log(err2)
+				})
+		})
+		.catch((err2) => {
+			console.log(err2)
+		})
 	})
 	.catch((err) => {
 		console.log(err)
