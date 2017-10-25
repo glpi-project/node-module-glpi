7b3b7eba093aae4dba0d477e2e2d9a4925abba2a
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-25 17:54:08 -0400

    build(lint): add eslint and mocha-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "indent": ["error", 4],
+        "camelcase": 0
+    },
+    "env": {
+        "mocha": true,
+        "node": true
+    }
+};
