58bd8bcf8d4a24b6382000eb75e258578fcfc410
Gianfranco Manganiello <gmanganiello@teclib.com>
2017-10-26 09:27:28 -0400

    ci(circle): add USER_EMAIL into config file

--- ci/create-config-file.sh
+++ ci/create-config-file.sh
@@ -25,6 +25,7 @@
 mv ./{config.example,config}.json
 sed -i -e "s|http://example.com/glpi/apirest.php|${APIREST_URL}|g" config.json
 sed -i -e "s|exampleName|${USER_NAME}|g" config.json
+sed -i -e "s|exampleEmail|${USER_EMAIL}|g" config.json
 sed -i -e "s|examplePassword|${USER_PASSWORD}|g" config.json
 sed -i -e "s|exampleUserToken|${USER_TOKEN}|g" config.json
 sed -i -e "s|exampleAppToken|${APP_TOKEN}|g" config.json
